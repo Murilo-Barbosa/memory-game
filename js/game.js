112bd5b34f44f66592fc2409a65c2df0c23556be
Murilo Barbosa <murilobarbosadarosa2002@gmail.com>
2022-09-14 12:18:15 -0300

    Adicionado novas funções

---
+++ js/game.js
@@ -0,0 +1,33 @@
+const grid = document.querySelector(".grid");
+
+const cards = [
+  "beth",
+  "brain",
+  "jerry",
+  "jessica",
+  "meeseeks",
+  "morty",
+  "pessoa-passaro",
+  "pickle-rick",
+  "rick",
+  "scroopy",
+  "summer",
+];
+
+// Função responsavel por criar um elemento de carta
+const createElement = (tag, className) => {
+  const element = document.createElement(tag);
+  element.className = className;
+  return element;
+};
+//Função responsavel por criar a carta!
+const createCard = () => {
+  const card = createElement("div", "card");
+  const front = createElement("div", "face front");
+  const back = createElement("div", "face back");
+
+  card.appendChild(front);
+  card.appendChild(back);
+
+  return card;
+};
